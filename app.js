b47b1ef08c2bb3277d54b95f7f9676bdbf263d1a
Will-2332 <willian.musquim@gmail.com>
2022-07-21 10:30:21 +0100

    i hate frontend @_@

--- app.js
+++ app.js
@@ -14,18 +14,18 @@ const db = require('./models/db');
 require('dotenv').config();
 
 // //   settings ? not sure yet
-const path = require("path");
-app.use('/static', express.static(path.join(__dirname + './views')));
 app.set('view engine', 'ejs');
-app.use('/css', express.static(__dirname + './views/css'));
-app.use('/images', express.static(__dirname + './views/images'));
-app.use('/views', express.static(__dirname + './views'))
-app.use('favicon.ico', express.static(__dirname + './favicon.ico'));
-app.use('/views', express.static(path.join(__dirname + './views')));
+const path = require("path");
+// app.use('/static', express.static(path.join(__dirname + './views')));
+// app.use('/css', express.static(__dirname + './views/css'));
+// app.use('/images', express.static(__dirname + './views/images'));
+// app.use('/views', express.static(__dirname + './views'))
+// app.use('favicon.ico', express.static(__dirname + './favicon.ico'));
+// app.use('/views', express.static(path.join(__dirname + './views')));
 
 // routes
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('index')
 });
 
 app.get('/search', (req, res) => {
