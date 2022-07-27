9d79848888fd55ecaf02cb5f5f513949de642559
Will-2332 <willian.musquim@gmail.com>
2022-07-27 15:10:31 +0100

    can't post with ajax :(

--- views/js/search.js
+++ views/js/search.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const mysql = require('mysql2/promise');
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const ejs = require("ejs");
-var urlencodedParser = require('urlencoded-parser')
-const { response } = require("express");
-
 document.getElementById('accommodation_search').addEventListener('click', e => {
     const location = document.getElementById('accommodation_location').value;
     ajaxSearch(location);
@@ -14,26 +6,33 @@ document.getElementById('accommodation_search').addEventListener('click', e => {
 async function ajaxSearch(location) {
     const ajaxResponse = await fetch(`/location/${location}`);
     const accommodation = await ajaxResponse.json();
-    document.getElementById('accommodation_results').innerHTML = '',
+    document.getElementById('accommodation_results').innerHTML = "",
         accommodation.forEach(accommodation => {
             const p = document.createElement('p');
-            const text = document.createTextNode(`${accommodation.name}${accommodation.location}${accommodation.type}`);
+            const text = document.createTextNode(`Name : ${accommodation.name}
+            Type : ${accommodation.type}`);
             p.appendChild(text);
             const btn = document.createElement('input');
             btn.setAttribute('type', 'button');
             btn.setAttribute('value', 'Book now!');
-            btn.addEventListener('click', async(e) => {
-                const ajaxReservation = await fetch(`/book/${accommodation.ID}`, {
-                    method: 'POST'
+            btn.addEventListener('click', async (e) => {
+                const ajaxReservation = await fetch('/book', {
+                    method: 'POST',
+                    data: {
+                        "accID": 1,
+                        "npeople": 1,
+                        "thedate": 20220801,
+                        "ID": accommodation.id
+                    }
                 })
-        if(ajaxReservation.status == 200){
-                alert('Reservation done');
-            } else {
-                const json = await response.json();
-                alert(`Error during reservation : ${json.error}`);
-            }
+                if (ajaxReservation.status == 200) {
+                    alert('Reservation done');
+                } else {
+                    const json = await response.json();
+                    alert(`Error during reservation : ${json.error}`);
+                }
+            });
+            document.getElementById('accommodation_results').appendChild(p);
+            document.getElementById('accommodation_results').appendChild(btn);
         });
-    document.getElementById('accommodation_results').appendChild(p);
-    document.getElementById('accommodation_results').appendChild(btn);
-});
 }
